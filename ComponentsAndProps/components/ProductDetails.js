940a3e909364ff987f8ce510de8f4f8ceed00e4a
jcoggins <jcog306749@earthlink.net>
2020-10-11 23:23:51 -0400

    Components and Props

---
+++ ComponentsAndProps/components/ProductDetails.js
@@ -0,0 +1,14 @@
+// solution
+app.component('product-details', {
+    props: {
+      details: {
+        type: Array,
+        required: true,
+      }
+    },
+    template:
+    /*html*/
+    `<ul>
+      <li v-for="detail in details">{{ detail }}</li>
+    </ul>`
+  })
